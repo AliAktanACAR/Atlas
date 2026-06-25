c73a1be406e3858f99014d78f44ad30b97350d88
AliAktanACAR <137204346+AliAktanACAR@users.noreply.github.com>
2026-06-25 15:11:17 +0300

    Add MathConstants class with mathematical constants

---
+++ src/math/MathConstants.java
@@ -0,0 +1,22 @@
+package atlas.math;
+
+public final class MathConstants {
+
+    private MathConstants() {}
+
+    public static final double PI = Math.PI;
+    public static final double TWO_PI = Math.PI * 2.0;
+    public static final double HALF_PI = Math.PI / 2.0;
+    public static final double QUARTER_PI = Math.PI / 4.0;
+
+    public static final double DEG_TO_RAD = Math.PI / 180.0;
+    public static final double RAD_TO_DEG = 180.0 / Math.PI;
+
+    public static final double EPSILON = 1e-9;
+
+    public static final double GRAVITY = 9.80665;
+
+    public static final double SQRT_2 = Math.sqrt(2);
+    public static final double SQRT_3 = Math.sqrt(3);
+
+}
